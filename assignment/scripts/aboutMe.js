e6be2b0ad79fd45f9f778d1653cc40f51fce6268
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-05 14:20:13 -0500

    reviewed and ready for submission

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,31 +1,43 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Jacob';
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Larson';
+
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + ' ' + lastName
+let fullName = `${firstName} ${lastName}`;
+
 // 4 - Console log the value of `fullName`
 console.log(fullName);
+
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = '69420';
+let luckyNumber = 69420;
+
 // 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-console.log('My name is ' + fullName + ', and I think ' + luckyNumber + ' is a winner!');
+console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner!`);
+
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
+
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'Pho';
+
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 2;
+
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 1;
+
 // 11 - Add two pets to your `pets` variable
-pets++
+pets+=2
+
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 const allowedPets = 69420;
+
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 if (adventurous === true) {
@@ -39,6 +51,7 @@ if (adventurous === true) {
 if (luckyNumber === 2 && adventurous === true) {
   console.log('Roll the dice!');
 }
+
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
@@ -51,6 +64,8 @@ if (pets < allowedPets) {
 } else if (pets > allowedPets) {
   console.log("Oh no, I have too many pets!");
 }
+
+
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -58,17 +73,21 @@ if (pets < allowedPets) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
-let mostPets;
+
+//Code written to handle ties.
+//friendsPets = 4
+let mostPets = pets;
 
 if (pets > friendsPets) {
     mostPets = pets;
     console.log(mostPets)
 } else if (pets < friendsPets) {
     mostPets = friendsPets
-    console.log(mostpets);
+    console.log(mostPets);
 } else {
-    console.log('Same same');
+    console.log('It\'s a tie!');
 }
+
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
 //https://www.youtube.com/watch?v=JTFSZhbpQ9A
 switch (true) {
@@ -77,15 +96,16 @@ switch (true) {
     console.log(mostPets);
     break;
   case (pets < friendsPets):
-    mostPets = friendsPets
+    mostPets = friendsPets;
     console.log(mostPets);
     break;
+  case (pets === friendsPets):
+    console.log('It\'s a tie!');
+    break;
 }
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 //https://www.youtube.com/watch?v=s4sB1hm73tw
-pets = 7;
-friendsPets = 8;
   (mostPets > pets) ? mostPets : (mostPets = pets);
   (mostPets > friendsPets) ? mostPets : (mostPets = friendsPets);
-
-console.log(mostPets);
+  (pets === friendsPets) ? console.log('It\'s a tie!') : console.log(mostPets);
